500d220316be658c98b56d145149529571b970e0
Adrian Zgrzebski <adrianski18@gmail.com>
2021-04-05 16:13:03 +0200

    Typo

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE "vehicles" (
 	"x" REAL NOT NULL,
 	"y" REAL NOT NULL,
 	"z" REAL NOT NULL,
-	"rotatation_x" REAL NOT NULL,
-	"rotatation_y" REAL NOT NULL,
-	"rotatation_z" REAL NOT NULL
+	"rotation_x" REAL NOT NULL,
+	"rotation_y" REAL NOT NULL,
+	"rotation_z" REAL NOT NULL
 );
