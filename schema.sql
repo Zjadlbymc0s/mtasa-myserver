f019ca3efdd0136e891c0996528a65e3c4b3b898
Adrian Zgrzebski <adrianski18@gmail.com>
2021-04-05 15:57:47 +0200

    Add initial database schema

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "vehicles" (
+	"id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"model" INTEGER NOT NULL,
+	"x" REAL NOT NULL,
+	"y" REAL NOT NULL,
+	"z" REAL NOT NULL,
+	"rotatation_x" REAL NOT NULL,
+	"rotatation_y" REAL NOT NULL,
+	"rotatation_z" REAL NOT NULL
+);
